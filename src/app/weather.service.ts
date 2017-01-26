b531a489f3cd3f2a8f54b64804e0322981398d3f
alvaro911 <algp232@hotmail.com>
2017-01-26 09:51:23 -0700

    get data from openweather app api

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+
+import { Weather } from './weather';
 
 @Injectable()
 export class WeatherService {
-  constructor() { }
+  private weather:Weather = new Weather('Provo', '100', 'sunny', '50', '200', '1:20', '5:20') ;
+  constructor(private http:Http) { }
 
-  getWeather(coord){
-    const lat = coord.latitude;
-    const long = coord.longitude;
+  currentLocation(lat:string, lon:string){
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?appid=0f3fb9fa31ad3d41f1bb2bd0841c3f2f&lat=${lat}&lon=${lon}&units=imperial`).map((response:Response) => response.json());
+
+    // return this.weather;
   }
 
-  currentLocation(){
-    navigator.geolocation.getCurrentPosition((pos) => {
-      return this.getWeather(pos.coords)
-    });
+  otherWeather(city:string){
+    return this.http.get(`http://api.openweathermap.org/data/2.5/weather?appid=0f3fb9fa31ad3d41f1bb2bd0841c3f2f&q=${city}&units=imperial`).map((response:Response) => response.json());
+  }
+
+  dummyWeather(){
+    return this.weather;
   }
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import 'rxjs/Rx';
 
 import { WeatherService } from '../weather.service';
+import { Weather } from '../weather';
 
 @Component({
   selector: 'wp-weather',
@@ -8,11 +11,34 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
-  pos:any;
+  weather:Weather
+  title = 'hello'
   constructor(private weatherSer:WeatherService) { }
-
+  weatherClass:Weather;
+  location:any = {};
   ngOnInit() {
-    console.log(this.pos.weatherSer.currentLocation())
+    this.weatherClass = this.weatherSer.dummyWeather();
+    navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
   }
 
+  setPosition(position){
+    this.location=position.coords;
+    const lat = this.location.latitude.toString();
+    const lon = this.location.longitude.toString();
+    this.weatherSer.currentLocation(lat, lon).subscribe(
+      (data) => {
+        this.weatherClass = new Weather(data.name, data.main.temp, data.weather[0].description, data.main.temp_min, data.main.temp_max, data.sys.sunrise, data.sys.sunset);
+        console.log(data);
+      }
+    )
+  }
+
+  onSubmit(weatherForm: NgForm){
+    this.weatherSer.otherWeather(weatherForm.value.city).subscribe(
+      (data) => {
+        this.weatherClass = new Weather(data.name, data.main.temp, data.weather[0].description, data.main.temp_min, data.main.temp_max, data.sys.sunrise, data.sys.sunset);
+        console.log(this.weatherClass);
+      }
+    )
+  }
 }
